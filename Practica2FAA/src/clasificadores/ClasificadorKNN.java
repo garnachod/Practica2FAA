dbee5bc41f1eefb061212b33f09789da670cfc4a
dowrow <dowrow@outlook.com>
2014-10-24 10:26:20 +0200

    Cálculo de máximos y mínimos

--- Practica2FAA/src/clasificadores/ClasificadorKNN.java
+++ Practica2FAA/src/clasificadores/ClasificadorKNN.java
@@ -2,6 +2,7 @@ package clasificadores;
 
 import datos.Datos;
 import datos.Elemento;
+import datos.TiposDeAtributos;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -12,6 +13,8 @@ import java.util.Collections;
 public class ClasificadorKNN extends Clasificador {
     
     Elemento datosTrain[][];
+    double maximos[];
+    double minimos[];
     
     @Override
     public void entrenamiento(Datos datosTrain) {
@@ -26,15 +29,61 @@ public class ClasificadorKNN extends Clasificador {
             Collections.addAll(ambos, datosTrain.getDatos());
             this.datosTrain = ambos.toArray(new Elemento[ambos.size()][]);
         }
+        
+        // Recalcular máximos y mínimos
+        this.maximos = new double[this.datosTrain[0].length];
+        this.minimos = new double[this.datosTrain[0].length];
+        this.calcularMaxMin();
     }
-
-    private void normalizar() {
-        // 
+    
+    private double maximo (int columna) {
+        double max = -Double.MAX_VALUE;
+        for (Elemento[] dato : this.datosTrain) {
+            if (dato[columna].getValorContinuo() > max) {
+                max = dato[columna].getValorContinuo();
+            }
+        }
+        return max;
+    }
+    
+    private double minimo (int columna) {
+        double min = Double.MAX_VALUE;
+        for (Elemento[] dato : this.datosTrain) {
+            if (dato[columna].getValorContinuo() < min) {
+                min = dato[columna].getValorContinuo();
+            }
+        }
+        return min;
+    }
+    
+    private void calcularMaxMin() {
+        
+        if (this.datosTrain == null)
+            return;
+        
+        // Para cada columna
+        for (int i = 0; i < this.datosTrain[0].length; i++) {
+            
+            // Si es de tipo continuo
+            if (datosTrain[0][i].getTipo() == TiposDeAtributos.Continuo) {
+                
+                // Encontrar máximo y mínimo
+                this.maximos[i] = maximo(i);
+                this.minimos[i] = minimo(i);
+            }
+            
+        }
+    }
+    
+    public double distancia (Elemento[] filaA, Elemento[] filaB) {
+        double distancia = 0;
+        
+        return distancia;
     }
     
     @Override
     public ArrayList<Elemento> clasifica(Datos datosTest) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
     }
     
 }
